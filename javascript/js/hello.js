b180db6f0bf67cb251866a784dd402d3fb680d20
A3loko <lokesh09rishi@gmail.com>
2018-02-07 17:30:19 +0530

    a

--- javascript/js/hello.js
+++ javascript/js/hello.js
@@ -1,1 +1,7 @@
-alert("Hello, World!!");
+//alert("Hello, World!!");
+
+var a =10;
+
+if(a > 5){
+  var b = 5;
+}
---
+++ javascript/js/new.js
@@ -0,0 +1,18 @@
+var x = function ()
+{
+  for(var a  = 0;a <10;a++)
+  {
+    if(true)
+    {
+      let  b = 8;
+    }
+    var c = a+b;
+    alert(c);
+    a++;
+  }
+
+
+}
+
+
+x();
